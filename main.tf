3909111c136fe9b141e58e47bdd78c77206374fb
endrit-albarjami <albarjami_endrit@live.com>
2025-08-21 16:02:40 +0200

    Variables

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "app_security_group" {
   source      = "terraform-aws-modules/security-group/aws//modules/web"
   version     = "4.17.0"
 
-  name        = "web-sg-project-alpha-dev"
+  name        = "web-sg-${var.resource_tags["project"]}-${var.resource_tags["environment"]}"
   description = "Security group for web-servers with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
@@ -44,7 +44,7 @@ module "lb_security_group" {
   source      = "terraform-aws-modules/security-group/aws//modules/web"
   version     = "4.17.0"
 
-  name        = "lb-sg-project-alpha-dev"
+  name        = "lb-sg-${var.resource_tags["project"]}-${var.resource_tags["environment"]}"
   description = "Security group for load balancer with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
@@ -63,8 +63,7 @@ module "elb_http" {
   version = "4.0.1"
 
   # Ensure load balancer name is unique
-  name = "lb-${random_string.lb_id.result}-project-alpha-dev"
-
+  name = "lb-${random_string.lb_id.result}-${var.resource_tags["project"]}-${var.resource_tags["environment"]}"
   internal = false
 
   security_groups = [module.lb_security_group.security_group_id]
